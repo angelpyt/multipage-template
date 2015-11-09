47f8e6c080907216f63da37baaebf79d5b238203
Yutong Pan <yutonp@uw.edu>
2015-11-09 09:41:19 -0800

    change

--- js/app.js
+++ js/app.js
@@ -1,13 +1,37 @@
 // Create app
-
+var myApp = angular.module('myApp', ['ui.router'])
 // Configure app
+myApp.config(function($stateProvider) {
+    $stateProvider
+    .state('home', {
+        url:'/home',
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController',
+    })
+    .state('about', {
+        url:'/about',
+        templateUrl: 'templates/about.html',
+        controller: 'AboutController'
+    })
+    .state('content', {
+        url:'/content',
+        templateUrl: 'templates/content.html',
+        controller: 'ContentController'     
+    })
+})
 
 // Landing page controller: define $scope.number as a number
+    .controller('HomeController', function($scope){
+      $scope.number = 6
+    })
 
 
 // About page controller: define $scope.about as a string
-
+    .controller('AboutController', function($scope){
+      $scope.about = 'Some info string'
+    })
 
 // Content controller: define $scope.url as an image
-
-
+    .controller('ContentController', function($scope){
+      $scope.url = "http://www.quicksprout.com/images/foggygoldengatebridge.jpg"
+    })
